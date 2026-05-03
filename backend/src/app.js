f4ac5a43a3e53fcaa78010452da8530af3fcfde3
hasanfadli <maskevet@gmail.com>
2026-05-04 01:45:46 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,13 @@ import cors from "cors";
 import routes from "./routes/index.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 
-app.listen(process.env.PORT||8080,()=>console.log("API running"));
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("API running on port", PORT);
+});
